1331bfc29fbebcdf3d4ff13752d121b11516e81f
John Scoles <byterock@hotmail.com>
2012-05-04 15:09:22 -0400

    ready for CPAN

--- lib/Mojolicious/Plugin/Authorization.pm
+++ lib/Mojolicious/Plugin/Authorization.pm
@@ -64,7 +64,7 @@ version 1.01
         'user_privs' => sub { ... },
         'user_role'  => sub { ... },
     });
-    if ($self->has('delete_all', { optional => 'extra data stuff' })) {
+    if ($self->has_priv('delete_all', { optional => 'extra data stuff' })) {
         ...
     }
 =head1 DESCRIPTION
@@ -76,41 +76,41 @@ you have already been authenticated and your role set.
 That is about it you are free to implement any system you like.
 =head1 METHODS
 =head2 has_priv('privilege', $extra_data) or has_privilege('privilege', $extra_data)
-Has_priv and has_privilege will use the supplied C<has_priv> subroutine ref to check if the current session has the
+'hHas_priv'' and ''has_privilege'' will use the supplied C<has_priv> subroutine ref to check if the current session has the
 given privilege. Returns true when the session has the privilege or false otherwise.
-You can pass additional data along in the extra_data hashref, it will be passed to your C<has_priv>
+You can pass additional data along in the extra_data hashref and it will be passed to your C<has_priv>
 subroutine as-is.
-=head2 is_role('role',$extra_data)
-Is_role will use the supplied C<is_role> subroutine ref to check if the current session is the
+=head2 is('role',$extra_data)
+'is' will use the supplied C<is_role> subroutine ref to check if the current session is the
 given role. Returns true when the session has privilege or false otherwise.
-You can pass additional data along in the extra_data hashref, it will be passed to your C<has_priv>
+You can pass additional data along in the extra_data hashref and it will be passed to your C<is_role>
 subroutine as-is.
-=head2 Privileges($extra_data)
-Privileges will use the supplied C<user_privs> subroutine ref and return the privileges of the current session.
-You can pass additional data along in the extra_data hashref, it will be passed to your C<has_priv>
+=head2 privileges($extra_data)
+'pPrivileges'' will use the supplied C<user_privs> subroutine ref and return the privileges of the current session.
+You can pass additional data along in the extra_data hashref and it will be passed to your C<user_privs>
 subroutine as-is. The returned data is dependant on the supplied C<user_privs> subroutine.
-=head2 Role($extra_data)
-Role will use the supplied C<user_role> subroutine ref and return the role of the current session.
-You can pass additional data along in the extra_data hashref, it will be passed to your C<has_priv>
-subroutine as-is. The returned data is dependant on the supplied C<user_privs> subroutine.
+=head2 role($extra_data)
+'role' will use the supplied C<user_role> subroutine ref and return the role of the current session.
+You can pass additional data along in the extra_data hashref and it will be passed to your C<user_role>
+subroutine as-is. The returned data is dependant on the supplied C<user_role> subroutine.
 =head1 CONFIGURATION
 The following options must be set for the plugin:
 =over 4
-=item has_priv (REQUIRED) A coderef for checking to see if the current session has a privilege (see L</"HAS">).
+=item has_priv (REQUIRED) A coderef for checking to see if the current session has a privilege (see L</"HAS PRIV">).
 =item is_role (REQUIRED) A coderef for checking to see if the current session is a certain role (see L</"IS ROLE">).
 =item user_privs (REQUIRED) A coderef for returning the privileges of the current session (see L</"PRIVILEGES">).
 =item user_role (REQUIRED) A coderef for retiring the role of the current session (see L</"ROLE">).
 =back
-=head1 HAS
-Has is used when you need to confirm that the current session has the given privilege.
+=head1 HAS PRIV
+'has_priv' is used when you need to confirm that the current session has the given privilege.
 The coderef you pass to the C<has_priv> configuration key has the following signature:
     sub {
         my ($app, $privilege,$extradata) = @_;
         ...
     }
 You must return either 0 for a fail and 1 for a pass.  This allows C<ROUTING VIA CONDITION> to work correctly.
-=head1 IS ROLE
-Is Role is used when you need to confirm that the current session is set to the given role.
+=head1 IS
+'is' is used when you need to confirm that the current session is set to the given role.
 The coderef you pass to the C<is_role> configuration key has the following signature:
     sub {
         my ($app, $role, $extradata) = @_;
@@ -119,7 +119,7 @@ The coderef you pass to the C<is_role> configuration key has the following signa
     }
 You must return either 0 for a fail and 1 for a pass.  This allows C<ROUTING VIA CONDITION> to work correctly.
 =head1 PRIVILEGES
-Privileges is used when you need to get all the privileges of the current session.
+'privileges' is used when you need to get all the privileges of the current session.
 The coderef you pass to the C<user_privs> configuration key has the following signature:
     sub {
         my ($app,$extradata) = @_;
@@ -129,7 +129,7 @@ The coderef you pass to the C<user_privs> configuration key has the following si
 You can return anything you want. It would normally be an arrayref of privileges but you are free to
 return a scalar, hashref, arrayref, blessed object, or undef.
 =head1 ROLE
-Role is used when you need to get the role of the current session.
+'role' is used when you need to get the role of the current session.
 The coderef you pass to the C<user_privs> configuration key has the following signature:
     sub {
         my ($app,$extradata) = @_;
@@ -166,7 +166,7 @@ L<Mojolicious::Sessions>, L<Mojocast 3: Authorization|http://mojocasts.com/e3#>
 =head1 AUTHOR
 John Scoles, C<< <byterock  at hotmail.com> >>
 =head1 BUGS / CONTRIBUTING
-Please report any bugs or feature requests through the web interface at L<https://github.com/benvanstaveren/mojolicious-plugin-Authorization/issues>.
+Please report any bugs or feature requests through the web interface at L<https://github.com/byterock/mojolicious-plugin-authorization/issues>.
 =head1 SUPPORT
 You can find documentation for this module with the perldoc command.
     perldoc Mojolicious::Plugin::Authorization
